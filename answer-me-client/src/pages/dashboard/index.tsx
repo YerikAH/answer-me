caedddf06da488b23c05fea6f0871f6a7ca2b095
Harvey Yerik <yerik05vh@gmail.com>
2024-09-15 17:16:00 -0500

    feat: :sparkles: Add Stat Section in Dashboard

--- answer-me-client/src/pages/dashboard/index.tsx
+++ answer-me-client/src/pages/dashboard/index.tsx
@@ -2,36 +2,8 @@ import { GreetHeader } from "./components/greet-header";
 import { useTitle } from "@/hooks";
 import { useEffect } from "react";
 import { TableLastCases } from "./components/table-last-cases";
-import { Stats } from "@/components";
-import { CircleStackIcon } from "@heroicons/react/20/solid";
 import { ChartSection } from "./components/chart-section";
-
-const stats = [
-  {
-    name: "Revenue",
-    value: "$405,091.00",
-    change: "+4.75%",
-    changeType: "positive",
-  },
-  {
-    name: "Overdue invoices",
-    value: "$12,787.00",
-    change: "+54.02%",
-    changeType: "negative",
-  },
-  {
-    name: "Outstanding invoices",
-    value: "$245,988.00",
-    change: "-1.39%",
-    changeType: "positive",
-  },
-  {
-    name: "Expenses",
-    value: "$30,156.00",
-    change: "+10.18%",
-    changeType: "negative",
-  },
-];
+import { StatsSection } from "./components/stats-section";
 
 export default function Dashboard() {
   const { changeTitle } = useTitle();
@@ -41,17 +13,7 @@ export default function Dashboard() {
   return (
     <div className="mx-auto w-full max-w-6xl mt-12">
       <GreetHeader />
-      <div>
-        <div className="flex justify-between items-center mt-8">
-          <h2 className="text-base font-semibold mt-6 mb-3 flex items-center gap-2">
-            <span className="bg-zinc-200 size-8 flex items-center justify-center rounded-full">
-              <CircleStackIcon className="text-zinc-900 size-4" />
-            </span>
-            Resumen
-          </h2>
-        </div>
-        <Stats stats={stats} />
-      </div>
+      <StatsSection />
       <ChartSection />
       <TableLastCases />
     </div>
